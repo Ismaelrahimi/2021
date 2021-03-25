4008379aad2368347c318dd816b0a3711a6a5096
ISMAEL RAHIMI <102576927@student.swin.edu.au>
2021-03-25 16:18:05 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace group1_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter number of time to reapeat:!");
+            string input2 = Console.ReadLine();
+            int number = int parse(input2)
+
+            int counter = 0;
+
+            while (counter < number)
+            {
+                Console.WriteLine(counter);
+                Console.WriteLine("Please Enter Your Name!");
+                string input = Console.ReadLine();
+
+                Console.WriteLine(input);
+
+                counter++;
+            }
+                Console.WriteLine("Goodbye");
+
+        }
+    }
+}
